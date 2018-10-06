0b20d7f670232281a2316b203097cf35fce620b8
Mohit Gokul Murali <43658499+mohitgokul@users.noreply.github.com>
2018-10-06 12:07:30 -0700

    Update JavaScript for Login Details.js

--- JavaScript for Login Details.js
+++ JavaScript for Login Details.js
@@ -1,21 +1,32 @@
 JavaScript for Login Details
 
+//Number of Attempts
 var attempt = 3;
-// Variable to count number of attempts.
+
 // Below function Executes on click of login button.
 function validate(){
+//Enter the Username
 var username = document.getElementById("username").value;
+//Enter the Password
 var password = document.getElementById("password").value;
-if ( username == "Formget" && password == "formget#123"){
+ 
+//Conditions if the user enters this Username and Password
+if ( username == "Formget" && password == "formget#123")
+{
 alert ("Login successfully");
 window.location = "success.html"; // Redirecting to other page.
+//Otherwise return Invalid
 return false;
 }
+
+//User gets one more Attempt
 else{
 attempt --;// Decrementing by one.
 alert("You have left "+attempt+" attempt;");
-// Disabling fields after 3 attempts.
-if( attempt == 0){
+  
+// No more try after three Attempts
+if( attempt == 0)
+{
 document.getElementById("username").disabled = true;
 document.getElementById("password").disabled = true;
 document.getElementById("submit").disabled = true;
